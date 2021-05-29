975d950f12e79428de3f9e192b0c75f1cce85093
ciscorx@gmail.com <ciscorx@gmail.com>
2021-05-29 13:18:38 -0500

    documentation revision

--- comment_out_ini_option.lua
+++ comment_out_ini_option.lua
@@ -298,7 +298,7 @@ end
 
 if not arg_with_flags or arg_with_flags["-h"] or arg_with_flags["--help"] then
    print [[
-Usage: Comments out an option in an ini file, if exists.  Pass the argument -u for uncomment and -c for comment out.  If there is a section header, it must be preceded by the -s flag.  The filename must be preceded with the flag -f.  Use -x to change the comment character from # to something else.  The arguments can follow any order.  Any spaces or parenthesis in the target text must be escaped.
+Usage: Comments out an option in an ini file, if exists.  Pass the argument -u for uncomment and -c for comment out.  If there is a section header, it must be preceded by the -s flag.  The filename must be preceded with the flag -f.  Use -x to change the comment character from # to something else.  The arguments can follow any order.  Any spaces or parenthesis in the target text must be escaped, if the argument is not delimited by quotes.
    For examples:
       sudo comment_out_ini_option.lua -f /boot/config.txt -u dtoverlay=disable-wifi
 
